f40f4a34891a2b743e873a8011515156d61f1847
Joan <joan35170@gmail.com>
2025-04-23 14:42:08 +0200

    Ajout des entites Soin et TypeSoin

---
+++ src/entity/Soin.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+//import {Pokemon} from './Pokemon';
+//import {TypeSoin} from './TypeSoin';
+
+@Entity({name: 'soins'})
+export class Soin {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({name: 'soin_date', type: 'date'})
+    soinDate: string;
+
+    @Column({type: 'text', nullable: true})
+    notes?: string;
+
+    @ManyToOne(() => Pokemon, (pokemon) => pokemon.soins, {onDelete: 'CASCADE'})
+    @JoinColumn({name: 'pokemon_id'})
+    pokemon: Pokemon;
+
+    @ManyToOne(() => TypeSoin, (typeSoin) => typeSoin.soins)
+    @JoinColumn({name: 'type_soin_id'})
+    typeSoin: TypeSoin;
+}
---
+++ src/entity/TypeSoin.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Soin } from './Soin';
+
+@Entity({ name: 'types_soins' })
+export class TypeSoin {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 50, unique: true })
+    nom: string;
+
+    @OneToMany(() => Soin, (soin) => soin.typeSoin)
+    soins: Soin[];
+}
