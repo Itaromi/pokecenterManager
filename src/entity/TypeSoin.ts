71bc47bef0e61999c580461f048481e1915d8271
youn <sylvestre.youn@gmail.com>
2025-04-25 16:23:31 +0200

    chiffrement de soins et TypeSoin.ts

--- src/entity/TypeSoin.ts
+++ src/entity/TypeSoin.ts
@@ -1,14 +1,40 @@
-import {Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToMany} from 'typeorm';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToMany,
+    BeforeInsert,
+    BeforeUpdate,
+    AfterLoad,
+} from 'typeorm';
 import { Soin } from './Soin';
+import { encrypt, decrypt } from '../utils/cryptoUtils';
 
 @Entity({ name: 'types_soins' })
 export class TypeSoin {
     @PrimaryGeneratedColumn('increment', { type: 'int', name: 'id' })
     id!: number;
 
-    @Column({ length: 50, unique: true })
+    @Column({ length: 512, unique: true }) // Augmente la taille pour le chiffrement
     nom!: string;
 
     @ManyToMany(() => Soin, (soin) => soin.typeSoin)
     soins!: Soin[];
+
+    // Champ déchiffré temporaire
+    private decryptedFields: Partial<Record<keyof TypeSoin, string>> = {};
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    encryptSensitiveData() {
+        this.nom = encrypt(this.nom);
+    }
+
+    @AfterLoad()
+    decryptSensitiveData() {
+        this.decryptedFields.nom = decrypt(this.nom);
+    }
+
+    // Getter pour accéder au nom déchiffré
+    getDecryptedNom() { return this.decryptedFields.nom; }
 }
